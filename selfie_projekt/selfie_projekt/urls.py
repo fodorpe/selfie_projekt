0cbd68c95bfcb4baa3f17f0c3bb7a0dfb1e3ef87
Péter Fodor <fodor.peter.21f@szlgbp.hu>
2026-01-15 21:48:06 +0100

    urls

--- selfie_projekt/selfie_projekt/urls.py
+++ selfie_projekt/selfie_projekt/urls.py
@@ -24,6 +24,19 @@ urlpatterns = [
     path('get-latest-overlay/', views.get_latest_overlay, name='get_latest_overlay'),
     path('kuldes/', views.kuldes, name='kuldes'),
 
+
+
+
+    path('raspberry-start-preview/', views.raspberry_start_preview, name='raspberry_start_preview'),
+    path('raspberry-stop-preview/', views.raspberry_stop_preview, name='raspberry_stop_preview'),
+    path('raspberry-get-preview/', views.raspberry_get_preview, name='raspberry_get_preview'),
+    path('raspberry-take-photo/', views.raspberry_take_photo, name='raspberry_take_photo'),
+
+
+
+
+
+
 ]
 
 
