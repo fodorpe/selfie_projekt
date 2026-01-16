bf7a38196faf44c4fb6a54cf345a4e02c7254a74
Péter Fodor <fodor.peter.21f@szlgbp.hu>
2026-01-16 14:58:18 +0100

    urls

--- selfie_projekt/selfie_projekt/urls.py
+++ selfie_projekt/selfie_projekt/urls.py
@@ -11,35 +11,32 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.email_view, name='email_view'),
-    path('selfie/', views.selfie_view, name='selfie_view'),
-    path('raspberry/', views.raspberry_camera_view, name='raspberry_camera'),
-    path('raspberry-take-photo/', views.raspberry_take_photo, name='raspberry_take_photo'),
-    # path('kuldes/', views.email_kuldes, name='email_kuldes'),
-    path('qr-code/', views.generate_qr_code, name='generate_qr_code'),
-    path('qr/', views.qr_display_view, name='qr_display'),
-    path('admin2/', views.admin2_view, name='admin2'),
-    path('test/', views.test_view, name='test'),
-    path('get-latest-overlay/', views.get_latest_overlay, name='get_latest_overlay'),
-    # path('kuldes/', views.kuldes, name='kuldes'),
+    # Selfie / email
+    path('', views.email_view, name='email'),
+    path('selfie/', views.selfie_view, name='selfie'),
 
-
-
-
-    path("capture/", views.capture_view, name="capture"),
-
-
+    # Raspberry Pi kamera
+    path('raspberry/', views.raspberry_view, name='raspberry'),
     path('raspberry-start-preview/', views.raspberry_start_preview, name='raspberry_start_preview'),
     path('raspberry-stop-preview/', views.raspberry_stop_preview, name='raspberry_stop_preview'),
     path('raspberry-get-preview/', views.raspberry_get_preview, name='raspberry_get_preview'),
     path('raspberry-take-photo/', views.raspberry_take_photo, name='raspberry_take_photo'),
 
+    # Fotók mentése / küldése emailben
+    path('kuldes/', views.kuldes, name='kuldes'),
+    path('email-kuldes/', views.email_kuldes, name='email_kuldes'),
 
+    # Overlay / QR
+    path('get-latest-overlay/', views.get_latest_overlay, name='get_latest_overlay'),
+    path('generate-qr/', views.generate_qr_code, name='generate_qr_code'),
+    path('qr-display/', views.qr_display_view, name='qr_display'),
 
+    # Admin & test
+    path('admin2/', views.admin2_view, name='admin2'),
+    path('test/', views.test_view, name='test'),
 
-
-
+    # Photo save API (ha van külön)
+    path('take-photo/', views.take_photo_view, name='take_photo'),
 ]
 
 
